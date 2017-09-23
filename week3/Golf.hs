c2a5e84abbb44a64ea03e80a7980945439ce0d0a
Alan Campora <alan.campora@gmail.com>
2017-09-23 12:16:22 -0300

    [MOD] Removing unncessary matching

--- week3/Golf.hs
+++ week3/Golf.hs
@@ -8,10 +8,12 @@ skips list  = map (every list) [1..length list]
 {-removing the elements before n, concat that element in the result list, 
  - then remove it and run the same process till the list have one element or when it's empty-}
 every :: [a] -> Int -> [a]
-every [] _ = []
 every list n 
     | length list > n = head reducedList : every (drop 1 reducedList) n
     | otherwise = reducedList
     where reducedList = drop (n-1) list
+
+
+
 
 

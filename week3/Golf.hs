4fbba02659f40e4d94242840d8ce12b78db4b9d6
Alan Campora <alan.campora@gmail.com>
2017-09-23 12:10:12 -0300

    [MOD] Ex1 was not working

--- week3/Golf.hs
+++ week3/Golf.hs
@@ -5,10 +5,13 @@ skips :: [a] -> [[a]]
 skips list  = map (every list) [1..length list] 
 
 
-{-removing the elements before n, concatenate that element in the result list, 
+{-removing the elements before n, concat that element in the result list, 
  - then remove it and run the same process till the list have one element or when it's empty-}
 every :: [a] -> Int -> [a]
-every [element] _ = [element]
 every [] _ = []
-every list n = head reducedList : every (drop 1 reducedList) n
+every list n 
+    | length list > n = head reducedList : every (drop 1 reducedList) n
+    | otherwise = reducedList
     where reducedList = drop (n-1) list
+
+
